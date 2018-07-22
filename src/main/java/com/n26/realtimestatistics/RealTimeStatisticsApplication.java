ecf1d573c5656d76e8ef1b7b0698f4ff83b46988
Gabriel Coelho <amorimdecoelho@gmail.com>
2018-07-22 21:22:09 +0200

    Initial commit with Spring Boot Seed

---
+++ src/main/java/com/n26/realtimestatistics/RealTimeStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.n26.realtimestatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeStatisticsApplication.class, args);
+	}
+}
